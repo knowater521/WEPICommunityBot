81a7ffeb552b59f21e23c94a1275a1dbf7aaa21f
SNAKE <xaz.snake0053@gmail.com>
2018-10-30 03:49:18 +0900

    Fix on_member_join
    メッセージ送信箇所修正

--- Bot.py
+++ Bot.py
@@ -15,8 +15,9 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel("480978992891953152")
-    string = member.mention + "さん、うぇぴ ゲーミングコミュニティへようこそ！\n\n #「うぇぴゲーミングコミュニティ」のご案内 には、ルールや必要事項が記載されています。入ったら最初ご覧になるようにお願いします。\n\n*新規の方は #verify にて` !verify`の発言をお願いしております。発言が確認され次第、自動的に他のチャンネルへの接続ができるようになります。*\n\n#自己紹介 は全ユーザーが共通して見ることができるチャンネルです。是非自己紹介をお願いします！\n\n---------------------------"""
-    await bot.say(string)
+    string = member.mention + "さん、うぇぴ ゲーミングコミュニティへようこそ！\n\n #「うぇぴゲーミングコミュニティ」のご案内 には、ルールや必要事項が記載されています。入ったら最初ご覧になるようにお願いします。\n\n*新規の方は #verify にて` !verify`の発言をお願いしております。発言が確認され次第、自動的に他のチャンネルへの接続ができるようになります。*\n\n#自己紹介 は全ユーザーが共通して見ることができるチャンネルです。是非自己紹介をお願いします！\n\n---------------------------"
+
+    await bot.send_message(channel, string)
 
 @bot.command(pass_context=True)
 async def verify(ctx):
